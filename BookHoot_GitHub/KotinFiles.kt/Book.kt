adb429a8de5f33595e856347046b9fbcf6b8ebf2
Sanstubh Katiyar <katiyarsanstubh@gmail.com>
2025-07-03 23:02:49 +0530

    Add files via upload

---
+++ BookHoot_GitHub/KotinFiles.kt/Book.kt
@@ -0,0 +1,16 @@
+package com.skyrist.bookhub
+
+data class Book (
+
+    val bookId: String,
+    val bookName: String,
+    val bookAuthor: String,
+    val bookRating: String,
+    val bookPrice: String,
+    val bookImage: String
+
+)
+
+
+
+
